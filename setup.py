796be858112b5a0a4789e82e3ff426f9d1ddfe82
chiragsanadhya <chiragsanadhya1411@gmail.com>
2024-07-10 22:58:38 +0530

    the second commit

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import find_packages, setup
+
+HYPEN_E_DOT = "-e ."
+
+def get_requirements(file_path: str):
+    '''this function will return list of requirements'''
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.strip() for req in requirements]
+        requirements = [req for req in requirements if not req.startswith(HYPEN_E_DOT)]
+    
+    return requirements
+
+setup(
+    name="ml_project",
+    version='0.0.1',
+    author='chirag',
+    author_email='chiragsanadhya1411@gmail.com',
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt')
+)
