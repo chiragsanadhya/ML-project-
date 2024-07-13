3dbd297a3b34c4caf0c1fa9330578787458659ea
chiragsanadhya <chiragsanadhya1411@gmail.com>
2024-07-13 14:46:16 +0530

    model trainer

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -10,6 +10,9 @@ from dataclasses import dataclass
 from src.components.data_transformation import DataTransformation
 from src.components.data_transformation import DataTransformationConfig
 
+from src.components.model_trainer import ModelTrainerConfig
+from src.components.model_trainer import ModelTrainer
+
 @dataclass
 class DataIngestionConfig:
     train_data_path = str=os.path.join('artifacts', "train.csv")
@@ -23,7 +26,7 @@ class DataIngestion:
     def initiate_data_ingestion(self):
         logging.info("Enter the data ingestion method of component")
         try:
-            df = pd.read_csv("/Users/chira/Desktop/NECESSITY/projects/ml_project/notebook/StudentsPerformance.csv")
+            df = pd.read_csv("/Users/chira/Desktop/NECESSITY/projects/ML-project-/notebook/StudentsPerformance.csv")
             
             logging.info('Read dataset as dataframe')
 
@@ -56,4 +59,7 @@ if __name__=="__main__":
     train_data,test_data = obj.initiate_data_ingestion()
 
     data_transformation = DataTransformation()
-    data_transformation.initiate_data_transformation(train_data,test_data)
+    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
+
+    modeltrainer=ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
